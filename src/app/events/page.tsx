095f9c7c3899435892770a6802da790a54a9fa1b
Dev-On-Repeat <ypranay56@gmail.com>
2026-09-23 19:59:42 +0530

    feat(pages): implement event exploration and dynamic search results pages

---
+++ src/app/events/page.tsx
@@ -0,0 +1,9 @@
+import EventList from "@/components/EventList";
+
+export default function EventsPage() {
+  return (
+    <main className="min-h-screen bg-gray-50/50">
+      <EventList />
+    </main>
+  );
+}
